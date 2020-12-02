0036235be49913dc95cc656d2c24a54c0ce05117
Georg Felix Dues <georg.duees@gmail.com>
2020-12-02 17:52:38 +0100

    day1 solution 1 and 2

--- AdventOfCode/Solutions/Year2020/Day01/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day01/Solution.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace AdventOfCode.Solutions.Year2020
@@ -8,18 +9,38 @@ namespace AdventOfCode.Solutions.Year2020
     class Day01 : ASolution
     {
 
-        public Day01() : base(01, 2020, "")
+        IEnumerable<int> Report;
+        public Day01() : base(01, 2020, "Report Repair")
         {
-
+           Report= Input.ToIntArray("\n").ToHashSet().Where(e=>e<2020);
         }
 
         protected override string SolvePartOne()
         {
-            return null;
+            var expenses = new Queue<int>(Report);
+            while(true){
+                var el=expenses.Dequeue();
+                foreach (var el2 in expenses)
+                {
+                    if(el+el2==2020)return (el*el2).ToString();
+                }
+            }
         }
 
         protected override string SolvePartTwo()
         {
+         var expenses = new Queue<int>(Report);
+            while(true){
+                var el = expenses.Dequeue();
+                foreach (var el2 in expenses)
+                {foreach (var el3 in expenses)
+                {
+                    if(el+el2+el3==2020)return(el*el2*el3).ToString();
+                    
+                }
+                    
+                }
+            }
             return null;
         }
     }

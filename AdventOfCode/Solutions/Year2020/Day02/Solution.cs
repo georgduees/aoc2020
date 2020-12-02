acbef6fccd093a9e7f97ebafffbf469e97968cab
Georg Felix Dues <georg.duees@gmail.com>
2020-12-02 19:31:12 +0100

    Day02 Commit

--- AdventOfCode/Solutions/Year2020/Day02/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day02/Solution.cs
@@ -12,62 +12,71 @@ namespace AdventOfCode.Solutions.Year2020
         public int minCount;
         public int maxCount;
         public string c;
-        public bool IsValid(){
-            var result = false;
-            var occurence = CountOccurence(ExamplePassword,this.c);
-            if(minCount<=occurence&&maxCount>=occurence){
-                result= true;
-            }
 
-            return result;
-        }
         public String ExamplePassword;
         //1-3 a: abcde
-        public int CountOccurence(string text,string pattern){
-            int c =0;
-            int i =0;
+
+        public PasswordPolicy(string InputText)
+        {
+            var parts = InputText.Split(" ");
+            this.minCount = Convert.ToInt32(parts[0].Split("-")[0]);
+            this.maxCount = Convert.ToInt32(parts[0].Split("-")[1]);
+            this.c = parts[1].Substring(0, 1);
+            this.ExamplePassword = parts[2];
+
+        }
+
+    }
+    class Day02 : ASolution
+    {
+        public int CountOccurence(string text, string pattern)
+        {
+            int c = 0;
+            int i = 0;
             while ((i = text.IndexOf(pattern, i)) != -1)
             {
-                i+=pattern.Length;
+                i += pattern.Length;
                 c++;
             }
             return c;
         }
-        public PasswordPolicy(string InputText){
-            var parts =InputText.Split(" ");
-            this.minCount=Convert.ToInt32(parts[0].Split("-")[0]);
-            this.maxCount=Convert.ToInt32(parts[0].Split("-")[1]);
-            this.c=parts[1].Substring(0,1);
-            this.ExamplePassword=parts[2];
-            
-        }
-        
-    }
-    class Day02 : ASolution
-    {
         string[] PasswordList;
-                public Day02() : base(02, 2020, "Password Philosophy")
+        public Day02() : base(02, 2020, "Password Philosophy")
         {
             PasswordList = Input.SplitByNewline().ToArray();
         }
 
         protected override string SolvePartOne()
         {
-          int counter =0;
-          for (var i = 0; i < PasswordList.Length; i++)
-          {
-           var cPassword = new PasswordPolicy(PasswordList[i]);
-           if(cPassword.IsValid()){
-               counter++;
-           }
-          }
+            int counter = 0;
+            for (var i = 0; i < PasswordList.Length; i++)
+            {
+                var cPassword = new PasswordPolicy(PasswordList[i]);
+                var occurence = CountOccurence(cPassword.ExamplePassword, cPassword.c);
+                if (cPassword.minCount <= occurence && cPassword.maxCount >= occurence)
+                {
+                    counter++;
+                }
+            }
             //var policy = new PasswordPolicy(InputList.FirstOrDefault());
             return counter.ToString();
         }
 
         protected override string SolvePartTwo()
         {
-            return null;
+            int counter = 0;
+            for (var i = 0; i < PasswordList.Length; i++)
+            {
+                var cp = new PasswordPolicy(PasswordList[i]);
+                char character = cp.c[0];
+                char loChar=cp.ExamplePassword[cp.minCount-1];
+                char hiChar=cp.ExamplePassword[cp.maxCount-1];
+                if((loChar==character)!=(hiChar==character)){
+                    counter++;
+                }
+            }
+            return counter.ToString();
+
         }
     }
 }
